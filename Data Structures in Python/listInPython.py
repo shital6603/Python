b84634888452e240f84f1a426d75b7a93217fa7b
Shital Khairnar <shital6603@gmail.com>
2024-01-03 21:17:41 +0530

    new list file creation

--- Data Structures in Python/listInPython.py
+++ Data Structures in Python/listInPython.py
@@ -2,3 +2,7 @@ listex=[56,"Client",'python']
 print(listex)
 listex.append([67,32,90])
 print(listex)
+print(listex[1])
+list2=['a','b','c','d','e','f','g','h','i']
+listex.extend(list2)
+print(listex)
