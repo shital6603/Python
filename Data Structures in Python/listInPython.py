ba03c3be346336e6d9118431b7d97afc0b53b672
Shital Khairnar <shital6603@gmail.com>
2024-01-03 21:12:17 +0530

    commit for new file

---
+++ Data Structures in Python/listInPython.py
@@ -0,0 +1,4 @@
+listex=[56,"Client",'python']
+print(listex)
+listex.append([67,32,90])
+print(listex)
--- additionOfTwoNumbers.py
+++
@@ -1,5 +0,0 @@
-print("Enter any two values :")
-val1 = int(input())
-val2 = int(input())
-sum = val1 + val2
-print("Addition of two numbwrs :",sum)
