6e48915baf1e2cd5c1bff69586174221631d611a
Shital Khairnar <78363561+shital6603@users.noreply.github.com>
2024-05-08 12:10:58 +0530

    Update printinMessage.py

--- printinMessage.py
+++ printinMessage.py
@@ -3,3 +3,10 @@ b= 200
 print("First number is ",a)
 print("First number is ",b)
 print("Addition of two numbers :",a+b)
+print("Are numbers equal????")
+print("Answer :")
+if a==b:
+  print("Both numbers are equal!!!")
+else:
+  print("Numbers are not equal")
+
